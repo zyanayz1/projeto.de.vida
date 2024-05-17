a11ca2a500714fb82fcd9e40dcff61970bc7072a
ana beatrizzz <166139508+zyanayz1@users.noreply.github.com>
2024-05-17 11:57:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-main.js
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes)
+for(let i=0; i<botoes.length; i=i+1){
+    botoes[i].onclick = function(){
+        botoes[i].classList.add("ativo")
+    }
+    console.log(i)
+}
